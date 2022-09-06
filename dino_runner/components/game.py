8c52d9a9e6ea635fa9227c3cd6d36f68c79f8d86
shumerezzz <112896554+shumerezzz@users.noreply.github.com>
2022-09-05 23:02:21 -0400

    Duck dinosaur
    Dinosaur  now can Duck

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
+from dino_runner.components.dinosaur import Dinosaur
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
 
 class Game:
     def __init__(self):
@@ -15,6 +15,8 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
+        self.player = Dinosaur()
+
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -23,19 +25,20 @@ class Game:
             self.update()
             self.draw()
         pygame.quit()
-
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
